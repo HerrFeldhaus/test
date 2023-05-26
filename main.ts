43c7d116bcfe13dcbc424c4ad528ef1d4a365040
HerrFeldhaus <115813982+HerrFeldhaus@users.noreply.github.com>
2023-05-26 21:58:31 +0200

    Vorbereitungen für den Ultraschallsensor

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 loops.everyInterval(60000, function () {
+	
+})
+basic.forever(function () {
     while (pins.analogReadPin(AnalogPin.C16) < 2) {
         basic.showString("KAFFEE!!!", 300)
     }
     basic.showString("Danke", 60000)
+})
+basic.forever(function () {
+	
 })
